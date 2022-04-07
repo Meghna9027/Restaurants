7c28e1997b953d6e31000c1cf7424d9dd8621276
Meghna Singh <96099699+Meghna9027@users.noreply.github.com>
2022-04-08 00:38:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Restaurants } from "./components/Restaurants";
+function App() {
+  return (
+    <div className="App">
+      <Restaurants />
+    </div>
+  );
+}
+
+export default App;
